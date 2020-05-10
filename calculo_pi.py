89b82cfc0055e393aec5de689a8b3a9afddc37f7
drent <henrryyanez@gmail.com>
2020-05-10 16:19:59 -0300

    Codigo validado y funcionando metodo Basilea

--- calculo_pi.py
+++ calculo_pi.py
@@ -1,10 +1,9 @@
+#Método de las Series (Basilea)
 import math
-"""
-Prueba de calculo de pi
-"""
+
 suma = 0
-for valor in range(1, 100000000):
+for valor in range(1, 1000):
     suma = suma + (1 / valor ** 2)
 
 pi = math.sqrt(suma * 6)
-print(f'El valor de pi es : {pi}')
+print(f'El valor de pi desde python es : {pi}')
